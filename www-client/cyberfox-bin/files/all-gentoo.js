7a9a423d0f788d0766d94c7364df4b8541ac35eb
logicoftekk <tekk@hotmail.ca>
2015-04-06 01:58:41 -0700

    dissable HeartBeat ratings system

--- www-client/cyberfox-bin/files/all-gentoo.js
+++ www-client/cyberfox-bin/files/all-gentoo.js
@@ -5,7 +5,8 @@ lockPref("intl.locale.matchOS",                true);
 // Allow user to change based on needs
 defaultPref("browser.display.use_system_colors",  true);
 defaultPref("spellchecker.dictionary_path", "/usr/share/myspell");
-defaultPref("browser.shell.checkDefaultBrowser",  false); 
+defaultPref("browser.shell.checkDefaultBrowser",  false);
+defaultPref("browser.selfsupport.url", "");
 // Preferences that should be reset every session
 pref("browser.EULA.override",              true); 
 
